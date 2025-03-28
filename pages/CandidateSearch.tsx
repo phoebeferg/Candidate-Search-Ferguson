711db40f756ca4094b5d1e91b077f36fdb7bb7b1
Phoebe <fergusonphoebe@gmail.com>
2025-03-27 22:11:36 -0400

    moved up a directory

--- pages/CandidateSearch.tsx
+++ pages/CandidateSearch.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
 import Candidate from '../interfaces/Candidate.interface';
